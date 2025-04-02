39c84a2fdb625750520b8583f253ca9e4d72ad09
ithodol <ddmdjesz1@gmail.com>
2025-04-02 11:16:15 +0900

    BaseTime 상속 클래스 적용

--- src/main/java/example/day02/_BaseTime/BaseTime.java
+++ src/main/java/example/day02/_BaseTime/BaseTime.java
@@ -2,6 +2,7 @@ package example.day02._BaseTime;
 
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -12,6 +13,7 @@ import java.time.LocalDateTime;
 // 해당 클래스는 일반 엔티티가 아닌 상속엔티티로 사용 한다.
 @MappedSuperclass // 해당 클래스는 일반 엔티티가 아닌 상속 엔티티로 사용한다
 @EntityListeners(AuditingEntityListener.class) // 해당 클래스의 멤버변수
+@Getter
 public class BaseTime {
 
     // 1. 엔티티/레코드 의 영속/생성 날짜/시간 자동 주입
