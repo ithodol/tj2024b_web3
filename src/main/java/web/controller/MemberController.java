e9dca066ac8e923df3483b215b013bd5ae56faa6
ithodol <ddmdjesz1@gmail.com>
2025-04-21 10:35:04 +0900

    HTTP 응답 (코드수정)

--- src/main/java/web/controller/MemberController.java
+++ src/main/java/web/controller/MemberController.java
@@ -2,6 +2,7 @@ package web.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import web.model.dto.MemberDto;
 import web.service.MemberService;
@@ -19,15 +20,32 @@ public class MemberController {
 
     // [1] 회원가입
     @PostMapping("/signup")
-    public boolean signup(@RequestBody MemberDto memberDto){
-        return memberService.signup(memberDto);
+//    public boolean signup(@RequestBody MemberDto memberDto){
+//        return memberService.signup(memberDto);
+//    } (수정 전)
+    public ResponseEntity<Boolean> signup(@RequestBody MemberDto memberDto){
+        boolean result = memberService.signup(memberDto);
+        if(result) {
+            // * 개발자가 원하는 응답 코드 반환 가능
+            return ResponseEntity.status(201).body(true); // 201(create ok 라는 뜻) true(반환값)
+        }else {
+            return ResponseEntity.status(400).body(false); // 400(bad request : 잘못된 요청)
+        }
     }
 
 
     // [2] 로그인
     @PostMapping("/login")
-    public String login(@RequestBody MemberDto memberDto){
-        return memberService.login(memberDto);
+//    public String login(@RequestBody MemberDto memberDto){
+//        return memberService.login(memberDto);
+//    }
+    public ResponseEntity<String> login(@RequestBody MemberDto memberDto){
+        String token = memberService.login(memberDto);
+        if(token != null){ // 만약 토큰이 존재하면(로그인 성공이면)
+            return ResponseEntity.status(200).body(token); // 200 코드 전송
+        }else {
+            return ResponseEntity.status(401).body("로그인 성공"); // 401(인증실패/권한없음)
+        }
     }
 
 
@@ -39,22 +57,38 @@ public class MemberController {
     // @PathVariable : HTTP 헤더의 경로 값 매핑하는 어노테이션
     // header : {'Authorization : 'yJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3NDQ3NzI5NTksImV4cCI6MzE3NDQzMTA4NjQ5OTU4N30.EmbdY71TV66hRLr6yP5VMaFBCDjDEI9JcFtLtH59LjY'}
     @GetMapping("/info")
-    public MemberDto info(@RequestHeader("Authorization") String token){
-        System.out.println("token : " + token);
-        return memberService.info(token);
+//    public MemberDto info(@RequestHeader("Authorization") String token){
+//        System.out.println("token : " + token);
+//        return memberService.info(token);
+//    }
+    public ResponseEntity<MemberDto> info(@RequestHeader("Authorization") String token){
+        MemberDto memberDto = memberService.info(token);
+        if(memberDto != null){
+            return ResponseEntity.status(200).body(memberDto); // 200 ok, 조회된 회원정보 반환
+        }else {
+            return ResponseEntity.status(403).build(); // 403 : noContent(자료없음)
+        }
     }
 
     // [4] 로그아웃 , 로그아웃 할 토큰 가져오기.
     @GetMapping("/logout")
-    public void logout(
-            @RequestHeader("Authorization") String token ) {
+//    public void logout(
+//            @RequestHeader("Authorization") String token ) {
+//        memberService.logout( token );
+//    }
+    public ResponseEntity<Void> logout(@RequestHeader("Authorization") String token ) {
         memberService.logout( token );
+        return ResponseEntity.status(204).build(); // 204 : 요청 성공했지만 반환값이 없다
     }
 
     // [5] 실시간 최근 24시간내 로그인 한 접속자 수
     @GetMapping("/login/count")
-    public int loginCount(){
-        return memberService.loginCount();
+//    public int loginCount(){
+//        return memberService.loginCount();
+//    }
+    public ResponseEntity<Integer> loginCount(){
+        int result = memberService.loginCount();
+        return ResponseEntity.status(200).body(result); // 200 : 요청 성공, 응답값 반환
     }
 
 }
